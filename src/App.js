ee17c24b5281fccbe512793b6f00131c8b6b80c4
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-10-02 19:05:04 -0500

    Completed Nested Routes. Instead of using Children in Layout component as Prop use Outlet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -10,12 +10,19 @@ function App() {
       <Navigation />
 
        {/* Next, we need to map the routes to an actual rendering by using 
-      React Router's Route component: */}
+       React Router's Route component: 
+       
+      */}
       
-      <Routes>
-        <Route path="home" element={<Home />} />
-        <Route path="users" element={<Users />} />
-        <Route path="about" element={<About />} />
+      {/* Note "path" attribute of Route component is directly match
+       "to" attribute of Link component*/}
+      <Routes> 
+         {/* it is possible to nest Route Component */}
+         <Route element={<Layout />}>
+            <Route path="home" element={<Home /> } />
+            <Route path="users" element={ <Users /> } />
+            <Route path="about" element={<About />} />
+        </Route>
       </Routes>
     </div>
   );
@@ -29,6 +36,16 @@ const Navigation = () => {
         paddingBottom: '1rem',
       }}
     >
+      {/* Note "to" attribute of Link compnent is directly match
+       "path" attribute of Route component
+
+        The essence of React Router is setting up Link components and matching them 
+      with Route components. 
+
+         Links have a many to one relationship to Routes, so that there can be multiple 
+      Links in your application linking to the same Route.
+
+       */}
       <Link to="/home">Home</Link>
       <Link to="/users">Users</Link>
       <Link to="/about">About</Link>
@@ -76,5 +93,18 @@ const Users = () => {
   );
 };
 
+//First let's create styling component
+//Now instead of using React's children in the Layout component, use React Router's 
+//Outlet component as equivalent:
 
+// const Layout = ({ children }) => {
+//   return <main style={{ padding: '1rem 0' }}>{children}</main>;
+// }
+const Layout = () => {
+  return (
+    <main style={{ padding: '1rem 0' }}>
+      <Outlet />
+    </main>
+  );
+};
 export default App;
